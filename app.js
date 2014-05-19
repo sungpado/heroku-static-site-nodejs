c1e6b92753a02d2fcc19bfb8f0852f8fd0750020
Gustavo Brito <gbritosampaio@gmail.com>
2014-05-19 00:36:37 -0300

    heroku port

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var app = connect()
     res.end('hello world\n');
   })
 
-console.log("Server listening on http://0.0.0.0:3000");
+var port = Number(process.env.PORT || 5000);
 
-http.createServer(app).listen(3000);
+console.log("Server listening on http://0.0.0.0:"+port);
+
+http.createServer(app).listen(port);
